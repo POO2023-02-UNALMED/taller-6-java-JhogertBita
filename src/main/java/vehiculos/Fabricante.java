7d7d7fce39ff4208b410aeee58bba80d23bb09c3
JhogertBita <141960659+JhogertBita@users.noreply.github.com>
2023-10-03 11:42:37 -0500

    Metodo fabricaMayorVentas

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -8,7 +8,7 @@ public class Fabricante {
 		this.nombre=nombre;
 		this.pais=pais;
 	}
-    
+
 	public String getNombre() {
 		return this.nombre;
 	}
@@ -21,4 +21,26 @@ public class Fabricante {
 	public void setPais(Pais pais) {
 		this.pais=pais;
 	}
+    public static Fabricante fabricaMayorVentas() {
+  		ArrayList<Fabricante> fabricantes=new ArrayList<Fabricante>();
+  		for (int i=0; i<Vehiculo.getVehiculos().size(); i++) {
+  			Fabricante x = Vehiculo.getVehiculos().get(i).getFabricante();
+  			if (fabricantes.contains(x)=false) {
+  				fabricantes.add(x);
+  			}
+  		}
+  		int lista[]; 
+  		lista=new int[fabricantes.size()];
+  		for (Vehiculo vehiculo: Vehiculo.getVehiculos()) {
+  			int indice=fabricantes.indexOf(vehiculo.getFabricante());
+  			lista[indice]++;
+  		}
+  		int n=0;
+  		for (int i=1; i<lista.length; i++) {
+  			if (lista[i]>lista[n]) {
+  				n=i;
+  			}
+  		}
+  		return fabricantes.get(n);
+  	}
 }
