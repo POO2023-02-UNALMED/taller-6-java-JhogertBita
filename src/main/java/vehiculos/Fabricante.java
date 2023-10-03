cc8e1836d11c8d083b1ce3ce956d74110379681d
JhogertBita <141960659+JhogertBita@users.noreply.github.com>
2023-10-03 11:36:36 -0500

    Creación Clase Fabricante

---
+++ src/main/java/vehiculos/Fabricante.java
@@ -0,0 +1,24 @@
+package vehiculos;
+import java.util.ArrayList;
+
+public class Fabricante {
+	private String nombre;
+	private Pais pais;
+	public Fabricante(String nombre,Pais pais) {
+		this.nombre=nombre;
+		this.pais=pais;
+	}
+    
+	public String getNombre() {
+		return this.nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	public Pais getPais() {
+		return this.pais;
+	}
+	public void setPais(Pais pais) {
+		this.pais=pais;
+	}
+}
