e1c7d0c084f9a5bf845e462162ff32a95a07c4c2
JhogertBita <141960659+JhogertBita@users.noreply.github.com>
2023-10-03 11:36:41 -0500

    Creación Clase Pais

---
+++ src/main/java/vehiculos/Pais.java
@@ -0,0 +1,16 @@
+package vehiculos;
+import java.util.ArrayList;
+
+public class Pais {
+  private String nombre;
+  public Pais(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getNombre() {
+    return this.nombre;
+  }
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+}
